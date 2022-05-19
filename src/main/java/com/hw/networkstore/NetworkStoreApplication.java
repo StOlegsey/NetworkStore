8f9b0954d7c7f6f18055ead1dbe8b2c8be48ccf5
StOlegsey <mgnedykh@gmail.com>
2022-05-19 20:29:24 +0300

    Initial commit

---
+++ src/main/java/com/hw/networkstore/NetworkStoreApplication.java
@@ -0,0 +1,13 @@
+package com.hw.networkstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetworkStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hw/networkstore/NetworkStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hw.networkstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
